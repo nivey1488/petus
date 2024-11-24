d01e40341bbdb47a29fc71ad3317a75558cc2353
nivey1488 <rusvadrusinvip@gmail.com>
2024-11-24 16:07:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,14 @@ function purchase(productName) {
     alert(`Спасибо за покупку!!!`);
 }
 let footer = document.getElementById('footer');
-let lastScrollTop = 0; // Последняя позиция прокрутки
+let lastScrollTop = 0;
 
 window.addEventListener('scroll', function() {
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
   if (scrollTop > lastScrollTop && scrollTop > 100) { 
-
-    footer.classList.add('hidden'); 
+    footer.classList.add('hidden');
   } else if (scrollTop < lastScrollTop) {
-
     footer.classList.remove('hidden'); 
   }
 
