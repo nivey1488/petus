19b940db969dbe5e1e3a9088ae1b84248665def3
nivey1488 <rusvadrusinvip@gmail.com>
2024-11-24 16:00:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-// Анимация при загрузке страницы
-window.addEventListener('load', () => {
-    document.body.classList.add('page-loaded');
-});
-
-// Функция для отображения сообщения при "покупке"
 function purchase(productName) {
     alert(`Спасибо за покупку!!!`);
 }
+let footer = document.getElementById('footer');
+let lastScrollTop = 0; // Хранит последнюю позицию прокрутки
+
+window.addEventListener('scroll', function() {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop && scrollTop > 100) { x
+    footer.classList.add('hidden'); 
+  } else {
+    footer.classList.remove('hidden'); 
+  }
+
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; 
+});
